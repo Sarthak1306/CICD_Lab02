e6baa6d67498cca5b7dddfbb4cb0371c35a5fd96
Sarthak Srivastava <sarthak13062001@gmail.com>
2021-08-16 15:53:18 +0530

    Update MyClass.java

--- src/main/java/MyClass.java
+++ src/main/java/MyClass.java
@@ -5,6 +5,7 @@ public class MyClass {
 		// TODO Auto-generated method stub
 		System.out.println("Hello");
 		System.out.println("Hello Git, Jenkins and Maven");
+		System.out.println("Additional Change for Poll SCM");
 	}
 
 }
